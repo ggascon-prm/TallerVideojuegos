38727f639b2653fdcc895ab2349a84e66c1d8015
ggascon <ggascon@gmail.com>
2022-06-17 10:42:21 +0200

    Added libs

--- src/main/java/com/germangascon/tallervideojuegos/Main.java
+++ src/main/java/com/germangascon/tallervideojuegos/Main.java
@@ -1,7 +1,13 @@
 package com.germangascon.tallervideojuegos;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("Mi juego");
+        frame.setSize(800, 600);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.pack();
     }
 }
